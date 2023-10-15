c579f0edcc41e7840df6a44d080623dca128dd47
Henry Leung <110437934+henryleung-greekisgood@users.noreply.github.com>
2023-10-16 00:58:49 +0100

    Update hardcodes.py

--- 3. test RealData/hardcodes.py
+++ 3. test RealData/hardcodes.py
@@ -1,2 +1,2 @@
-srcfolder = "/Users/yeehinleung/Documents/GreekIsGood/Products/TSFEFS/XAU_USD_demo"
-dstfolder = "/Users/yeehinleung/Documents/GreekIsGood/Products/TSFEFS/test RealData"
+srcfolder = ""
+dstfolder = ""
