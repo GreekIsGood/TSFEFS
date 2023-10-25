383c1d14b24cfd513a69606f3dbfd8632e625703
henryleung-greekisgood <henry.leung@greekisgood.com>
2023-10-25 11:01:08 +0100

    change hardcodes.py location

--- 3. test RealData/hardcodes.py
+++ 3. test RealData/hardcodes.py
@@ -1,2 +1,1 @@
-srcfolder = "/Users/yeehinleung/Documents/GreekIsGood/Products/TSFEFS/XAU_USD_demo"
-dstfolder = "/Users/yeehinleung/Documents/GreekIsGood/Products/TSFEFS/test RealData"
+../hardcodes.py
